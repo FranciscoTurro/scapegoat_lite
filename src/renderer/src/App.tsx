12a149358c5949f25981ca0a3f493faa0e7f4231
FranciscoTurro <franciscogtrro@outlook.com.ar>
2026-03-30 14:45:51 -0300

    added shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,5 @@
+import { Button } from './components/ui/button'
+
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
@@ -5,6 +7,7 @@ function App(): React.JSX.Element {
     <div>
       <div>many such cases.</div>
       <div>get the cat.</div>
+      <Button>aaaaaaaaaaaaaaaaaa</Button>
     </div>
   )
 }
